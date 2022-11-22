019dd5694c167a6b3164afcaf87035160ef92ad8
IAmDanieI <mypratastastegood@gmail.com>
2022-11-22 17:59:49 +0800

    Added Bottom Navigation Menu

--- app/src/main/java/com/example/recipeapp/Home.java
+++ app/src/main/java/com/example/recipeapp/Home.java
@@ -1,37 +1,52 @@
 package com.example.recipeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class Home extends AppCompatActivity {
-
     private Button profile,feedback;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        profile = (Button) findViewById(R.id.profile);
-        profile.setOnClickListener(new View.OnClickListener() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Home.this,ProfileActivity.class));
-            }
-        });
-
-        feedback = (Button) findViewById(R.id.feedback);
-        feedback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // do something when feedback clicked
-                startActivity(new Intent(Home.this,FeedbackActivity.class));
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.navigation_home) {
+                   setContentView(R.layout.activity_home);
+                }
+                if (id == R.id.navigation_profile) {
+                    Intent navProfile = new Intent(Home.this, ProfileActivity.class);
+                    Home.this.startActivity(navProfile);
+                    return true;
+                }
+                if (id == R.id.navigation_feedback) {
+                    Intent navFeedback = new Intent(Home.this, FeedbackActivity.class);
+                    Home.this.startActivity(navFeedback);
+                    return true;
+                }
+                return false;
             }
         });
 
     }
+
 
 }
